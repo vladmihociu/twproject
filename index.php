fb072fb06e2c477d2225b1f560903a9ae0f9278c
Mihociu Vlad <vlad.maciu@gmail.com>
2017-04-20 16:31:35 +0300

    Changed the username and password font

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
-<?php
+<?php session_start();
 
 if(isset($_COOKIE['username'])) {
 
 	//header("Location: /php/chat.php");
 	header("Location: /home.html");
 }
+$current_date = date('Y-m-d H:i:s');
 
 ?>
 
@@ -14,19 +15,20 @@ if(isset($_COOKIE['username'])) {
 <head>
 <meta charset="UTF-8">
 <link type="text/css" rel="stylesheet" href="css/style.css" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Login Page</title>
 </head>
-<body>
+<body class="body_login">
 <form action="php/login.php" method='POST'>
   <div class="imgcontainer">
     <img src="photos/Login.png" alt="Avatar" class="avatar">
   </div>
 
-  <div class="container">
-    <label><b>Username</b></label>
+  <div class="container" style = "width:40%;margin:0 auto;">
+    <label><b>USERNAME:</b></label>
     <input type="text" placeholder="Enter Username" name="username" required>
 	<br>
-    <label><b>Password</b></label>
+    <label><b>PASSWORD:</b></label>
     <input type="password" placeholder="Enter Password" name="password" required>
     <br>
     <button type="submit">Login</button>
