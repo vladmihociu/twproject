5871dbb6a13a7cd491e254afb86374cc0893368e
Mihociu Vlad <vlad.maciu@gmail.com>
2017-06-09 18:16:35 +0300

    Edited the entire loggedusers php

--- php/loggedusers.php
+++ php/loggedusers.php
@@ -1,12 +1,17 @@
-<?php
-session_start();
+<?php include 'databaseconnection.php';
+					
+					$sql = "select username from loggedusers";
+					$result = $conn->query($sql);
 
-echo "Branza";
-if(isset($_SESSION["username"])){
-    $text = $_POST['text'];
-     
-    $fp = fopen("loggedusers.html", 'a');
-    fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".$_SESSION["username"]."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
-    fclose($fp);
-}
+					if($result === FALSE) { 
+					echo "Query invalid";
+					/* die(mysql_error()); // TODO: better error handling */
+				}					 
+					echo "<ul>";
+
+					while($row = mysqli_fetch_array($result)){
+						echo "<b>".$row['username']."</b>"."<br>";
+					}
+					echo "</ul>";
+					
 ?>
