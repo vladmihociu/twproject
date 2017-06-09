1a728c6b3be63aeae441e6c3458c7b39d309a3dd
Mihociu Vlad <vlad.maciu@gmail.com>
2017-06-09 18:18:28 +0300

    removed loggedcounter database management

--- php/logout.php
+++ php/logout.php
@@ -6,6 +6,8 @@ function logout()
 		$username = "root";
 		$password = "";
 		$dbname = "proiect";
+		
+		$user=$_SESSION["username"];
 
 		// Create connection
 		$conn = new mysqli($servername, $username, $password, $dbname);
@@ -14,24 +16,17 @@ function logout()
 		if ($conn->connect_error) {
 			die("Connection failed: " . $conn->connect_error);
 		} 
-		$sql = "UPDATE loggedcounter SET counter = counter - 1";
-		$result=null;
-		$result = $conn->query($sql);
-		$user=$_SESSION["username"];
-		$sql = "DELETE FROM loggedusers WHERE username='$user'";
-		$result = $conn->query($sql);
-		if($result)
-		{
-			return 1;
-		}
-		return 0;
 
+		$stmt = $conn->prepare('DELETE FROM loggedusers WHERE username=?');
+		$stmt->bind_param('s', $user);
+		$stmt->execute();
+		
+		return 1;
 	}
 	
 	if(logout())
 	{
 		setcookie('username', $_SESSION["username"], time() + (-86400), "/"); // 86400 = 1 day
-		echo "bram";
 		setcookie('password', $_SESSION["password"], time() + (-86400), "/"); // 86400 = 1 day			
 
 		header("Location: ../index.php");
